c7cd253f1b355b4c924c802d9f6816e8203b8906
kaushik <kaushikpatnaik@gmail.com>
2022-12-31 08:33:59 -0800

    Adding mock api for faster iteration

--- provided_api.py
+++ provided_api.py
@@ -1,62 +1,93 @@
-import os
-from google.auth.transport.requests import Request
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.oauth2.credentials import Credentials
-from googleapiclient.errors import HttpError
-from googleapiclient.discovery import build
+'''
+Mock interface to Google Calendar bcs permissions make iterations slower
+'''
+from datetime import datetime, timedelta
 
-SCOPES = ['https://www.googleapis.com/auth/calendar']
+now = datetime.now()
+current_date = datetime.now().date()
+tomorrow = current_date + timedelta(days=1)
+next_week = current_date + timedelta(weeks=1)
+two_weeks = current_date + timedelta(weeks=2)
+next_friday = now + timedelta( (4-now.weekday()) % 7 )
+test_meet_link = "https://meet.google.com/abc-def-ghi"
+test_meet_link2 = "https://meet.google.com/pqr-stu-vwx"
+
+# rescheduling and editing means the model needs access to current meetings via an API
+# the prompt will be need to be updated based on the information retrieved in order to complete the plan
+# all_info_prior_meetings is the returned meeting information from the API
+all_info_prior_meetings = [
+    {
+        "summary": "Weekly design sync",
+        "location": ["Main Conference Room", test_meet_link],
+        "description": "This is a new meeting",
+        "start": {
+            "dateTime": (tomorrow + timedelta(days=3) + timedelta(hours=9)).strfttime("%Y-%m-%d %H:%M:%S"),
+            'timeZone': 'America/New_York',
+        },
+        'end': {
+            'dateTime': (tomorrow + timedelta(days=3) + timedelta(hours=9) + timedelta(mins=60)).strfttime("%Y-%m-%d %H:%M:%S"),
+            'timeZone': 'America/New_York',
+        },
+        "attendees": ["kaushik@gmail.com", "kabir@gmail.com", "ron@gmail.com", "patrick@gmail.com"],
+    },
+    {
+        "summary": "Weekly design sync",
+        "location": ["Main Conference Room", test_meet_link],
+        "description": "This is a new meeting",
+                "start": {
+            "dateTime": (next_week + timedelta(hours=9)).strfttime("%Y-%m-%d %H:%M:%S"),
+            'timeZone': 'America/New_York',
+        },
+        'end': {
+            'dateTime': (next_week + timedelta(hours=9) + timedelta(mins=60)).strfttime("%Y-%m-%d %H:%M:%S"),
+            'timeZone': 'America/New_York',
+        },
+        "attendees": ["kaushik@gmail.com", "kabir@gmail.com", "ron@gmail.com", "patrick@gmail.com"],
+    },
+    {
+        "summary": "Spring meeting",
+        "location": [test_meet_link],
+        "datetime": tomorrow + timedelta(hours=9),
+        "duration": 30,
+        "attendees": ["kaushik@gmail.com", "asiya@gmail.com", "ram@gmail.com", "arvind@gmail.com", "alex@gmail.com"],
+    },
+    {
+        "summary": "Discuss Eng roadmap",
+        "location": ["Secondary conference room", test_meet_link],
+        "datetime": current_date + timedelta(days=2) + timedelta(hours=10),
+        "duration": 60,
+        "attendees": ["kaushik@gmail.com", "asiya@gmail.com", "ram@gmail.com", "arvind@gmail.com", "alex@gmail.com"],
+     },
+     {
+        "summary": "Project proposal review",
+        "location": ["Secondary conference room", test_meet_link],
+        "datetime": next_friday + timedelta(hours=16),
+        "duration": 30,
+        "attendees": ["kaushik@gmail.com", "roh@gmail.com"],
+     },
+     {
+        "summary": "Product meeting",
+        "location": ["Secondary conference room", test_meet_link],
+        "datetime": two_weeks + timedelta(days=2),
+        "duration": 30,
+        "attendees": ["kaushik@gmail.com", "roh@gmail.com", "arvind@gmail.com", "alex@gmail.com"],
+     },
+     {
+        "summary": "Lunch with Rochit",
+        "location": ["Zareen's Palo Alto"],
+        "datetime": tomorrow + timedelta(hours=12),
+        "duration": 60,
+        "attendees": ["kaushik@gmail.com", "rochit@gmail.com"],
+     },
+]
 
 def name_to_emails(list_of_names):
     return [name+"@gmail.com" for name in list_of_names]
 
-def _setup_oauth():
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file(os.path.join(os.environ['secret_location'], 'token.json'), SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                os.path.join(os.environ['secret_location'], 'credentials.json'), SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open(os.path.join(os.environ['secret_location'], 'token.json'), 'w') as token:
-            token.write(creds.to_json())
-    
-    return creds
-
 def get_events():
-    creds = _setup_oauth()
-
-    # Create a service object
-    service = build('calendar', 'v3', credentials=creds)
-
-    # Set the time range for the events
-    time_min = '2022-12-24T00:00:00Z'
-    time_max = '2023-01-31T23:59:59Z'
-
-    # Call the Calendar API to retrieve the events
-    events_result = service.events().list(calendarId='primary', timeMin=time_min, timeMax=time_max, singleEvents=True, orderBy='startTime').execute()
-    events = events_result.get('items', [])
-
-    return events
+    return all_info_prior_meetings
 
 def create_event(meeting_agenda, date, time, duration, invitees):
-    # Replace with your own credentials
-    creds = _setup_oauth()
-
-    # Create a service object
-    service = build('calendar', 'v3', credentials=creds)
-
-    # Set the calendar ID
-    calendar_id = 'primary'
-
     # Set the meeting details
     event = {
         'summary': meeting_agenda,
@@ -76,25 +107,14 @@ def create_event(meeting_agenda, date, time, duration, invitees):
         },
     }
 
-    # Create the event
-    event = service.events().insert(calendarId=calendar_id, body=event).execute()
     return event
 
 def edit_event(event_id, meeting_agenda, date, time, duration, invitees):
-    # Replace with your own credentials
-    creds = _setup_oauth()
-
-    # Create a service object
-    service = build('calendar', 'v3', credentials=creds)
-
-    # Set the calendar ID
-    calendar_id = 'primary'
-
     # Set the meeting details
     event = {
         'summary': meeting_agenda,
         'location': 'Online',
-        'description': 'This is a new meeting',
+        'description': 'This is an edited meeting',
         'start': {
             'dateTime': f'{date}T{time}:00',
             'timeZone': 'America/New_York',
@@ -105,12 +125,5 @@ def edit_event(event_id, meeting_agenda, date, time, duration, invitees):
         },
         'attendees': [{'email': email} for email in invitees],
     }
-    
-    try:
-        updated_event = service.events().patch(calendarId='primary', eventId=event_id, body=event).execute()
-        return updated_event
-    except HttpError as error:
-        print("An error occured")
 
-if __name__ == "__main__":
-    create_event("Meet and Greet", "01/01/2023", "13:00", "60", ["scratchpad@gmail.com"])
+    return 
